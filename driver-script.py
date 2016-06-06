a3c829d2db75daa1eb5c280086b511e7100c3aa6
Logan Goldberg <goldberg@grinnell.edu>
2016-06-06 18:07:30 -0500

    added fields and finished setup_args

--- driver-script.py
+++ driver-script.py
@@ -2,28 +2,55 @@ import sys
 
 class jcc:
     """
-    
+    A class used to assist with gcc to llvm driver script.
     """
     
     def __init__(self, args):
+        """
+        Constructor for the class.
+        
+        Fields
+        
+        self.filename -- name of file to be converted to bytecode.
+        self.compfiles -- array of files to be compiled with or linked together.
+        self.prior_flags -- array of flags that precede the filename.
+        self.after_flags -- array of flags that succeed the files in compfiles.
+        """
         self.filename = ""
         self.compfiles = []
+        self.prior_flags = []
+        self.after_flags = []
         
+        self.setup_args(args)
         
+    def setup_args(self, args):
+        """
+        Method used to parse inputs of the command line into their respective arrays.
         
-    def setup_args(self):
+        Arguments:
+        args -- array of arguments to parse.
+        
+        Preconditions: args must be nonempty.
+        Postconditions: filename, compfiles, and prior_flags are nonempty.
+        """
         enumed_args = list(map(lambda x: [x, args.index(x)], args))
         flagged_args = list(filter(lambda x: x[:][0][0] == '-', enumed_args))
         
         while (flagged_args[0][0] != '-o'):
-            flagged_args.pop(0)
+            self.prior_flags.append(flagged_args.pop(0)[0])
         
-        lower_index_bound = flagged_args.pop(0)[1] + 1 # Lower_index_bound represents the filename, not '-o'
+        flag = flagged_args.pop(0) # Pop array containing '-o'
+        self.prior_flags.append(flag[0])
+        lower_index_bound = flag[1] + 1 # Lower_index_bound represents the filename, not '-o'
         
         if flagged_args == []: # Check if there are any flags after '-o'.
             upper_index_bound = len(args) # If there are no flags after '-o', upper bound is end of command line.
         else:
-            upper_index_bound = flagged_args.pop(0)[1] # Otherwise, upper bound is first flag after c files.
+            flag = flagged_args.pop(0) # Pop off next flag after files.
+            self.after_flags.append(flag[0])
+            upper_index_bound = flag[1] # Otherwise, upper bound is first flag after c files.
+
+        self.after_flags.extend(list(map(lambda x: x[0], flagged_args)))
         
         self.filename = args[lower_index_bound]
         
@@ -36,7 +63,14 @@ class jcc:
         
     def link_structures(self):
         pass
+    
+    def print_fields(self):
+        print self.filename
+        print self.compfiles
+        print self.prior_flags
+        print self.after_flags
         
         
 
 driver = jcc(sys.argv)
+driver.print_fields()
